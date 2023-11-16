a1a709f371de7f53e1c678bdda334d4acb22b387
Matteo Angelini <matteo.angelini@outlook.com>
2023-11-16 17:17:23 +0100

    FIX import

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -4,10 +4,11 @@ import {
   authenticate,
 } from '@loopback/authentication';
 import {inject} from '@loopback/core';
-import {repository} from '@loopback/repository';
+import {FilterExcludingWhere, repository} from '@loopback/repository';
 import {
   SchemaObject,
   del,
+  get,
   getModelSchemaRef,
   param,
   patch,
@@ -122,19 +123,19 @@ export class UserController {
     await this.userRepository.deleteById(id);
   }
 
-  // @get('/users/{id}')
-  // @response(200, {
-  //   description: 'User model instance',
-  //   content: {
-  //     'application/json': {
-  //       schema: getModelSchemaRef(User, {includeRelations: true}),
-  //     },
-  //   },
-  // })
-  // async findById(
-  //   @param.path.number('id') id: number,
-  //   @param.filter(User, {exclude: 'where'}) filter?: FilterExcludingWhere<User>,
-  // ): Promise<User> {
-  //   return this.userRepository.findById(id, filter);
-  // }
+  @get('/users/{id}')
+  @response(200, {
+    description: 'User model instance',
+    content: {
+      'application/json': {
+        schema: getModelSchemaRef(User, {includeRelations: true}),
+      },
+    },
+  })
+  async findById(
+    @param.path.number('id') id: number,
+    @param.filter(User, {exclude: 'where'}) filter?: FilterExcludingWhere<User>,
+  ): Promise<User> {
+    return this.userRepository.findById(id, filter);
+  }
 }
